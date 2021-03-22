bb51b2de8cce35587e216418039d79048f54a117
Arifian Saputra <arifiansaputra43@gmail.com>
2021-03-22 12:39:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
     var i=0,text;
     
-    var text = "You can find me on : ";
+    var text = "Hello ! ";
     
     function typing() {
         if(i<text.length){
             document.getElementById("text").innerHTML += text.charAt(i);
             i++;
-            setTimeout(typing,100);
+            setTimeout(typing,200);
         }
     }
     typing();
