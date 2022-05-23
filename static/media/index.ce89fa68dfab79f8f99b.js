981c694272b3b30c0fdea1d813ce0fe960094fcd
arifian853 <arifiansaputra43@gmail.com>
2022-05-23 14:44:01 +0700

    Updates

--- static/media/index.ce89fa68dfab79f8f99b.js
+++
@@ -1,1 +0,0 @@
-import React from"react";import ReactDOM from"react-dom";import"./index.css";import App from"./App";import reportWebVitals from"./reportWebVitals";import{jsx as _jsx}from"react/jsx-runtime";ReactDOM.render(_jsx(React.StrictMode,{children:_jsx(App,{})}),document.getElementById("root")),reportWebVitals();
