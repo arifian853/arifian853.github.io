a9e2c8e958951feac525ee7dda083461817d79be
arifian853 <arifiansaputra43@gmail.com>
2022-05-28 15:53:07 +0700

    Updates

---
+++ precache-manifest.75e9ad746b8c31865fb4ff9ee0c14b73.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "e419dff9bd935172b222",
+    "url": "/static/css/main.7a59ee5e.chunk.css"
+  },
+  {
+    "revision": "e419dff9bd935172b222",
+    "url": "/static/js/main.e419dff9.chunk.js"
+  },
+  {
+    "revision": "e7e0e04a5ea3842eb42e",
+    "url": "/static/js/1.e7e0e04a.chunk.js"
+  },
+  {
+    "revision": "f8ffe8ae9187ac1bd7b0",
+    "url": "/static/css/2.157f3184.chunk.css"
+  },
+  {
+    "revision": "f8ffe8ae9187ac1bd7b0",
+    "url": "/static/js/2.f8ffe8ae.chunk.js"
+  },
+  {
+    "revision": "6a4751f849122296e7c6",
+    "url": "/static/js/runtime~main.6a4751f8.js"
+  },
+  {
+    "revision": "d6f10f30d83d67d1e6e40a1b8673ed9b",
+    "url": "/static/media/4.d6f10f30.jpg"
+  },
+  {
+    "revision": "b9518d77bdd2dfb8bc84498d636db469",
+    "url": "/static/media/3.b9518d77.jpg"
+  },
+  {
+    "revision": "8120f97f826352bd7d330b64fc5f86e0",
+    "url": "/static/media/2.8120f97f.jpg"
+  },
+  {
+    "revision": "22c536d9a186754ec10cedce26c17fe0",
+    "url": "/static/media/1.22c536d9.jpg"
+  },
+  {
+    "revision": "fa74836cc07022ecf2f03b58388465ae",
+    "url": "/index.html"
+  }
+];
