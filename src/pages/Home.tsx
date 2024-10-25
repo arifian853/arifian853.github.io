eea3533e65981c53778e05c06625edc38b214e1a
arifian853 <arifiansaputra43@gmail.com>
2024-10-26 02:31:23 +0700

    Added new components

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,6 @@ import { Helmet } from "react-helmet"
 export const Home = () => {
     return (
         <>
-
             <Helmet>
                 <title>Arifian Saputra</title>
             </Helmet>
