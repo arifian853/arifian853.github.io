4622a60dc59dd49f2ff99a2e869244d3285a1caf
arifian853 <arifiansaputra43@gmail.com>
2024-11-10 23:50:11 +0700

    Added new scroll to top function

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -13,7 +13,7 @@ export const Chat = () => {
             </Helmet>
             <div data-aos="zoom-out" data-aos-duration='600' className="w-11/12 md:w-96 bg-[#BABFBF] dark:bg-[#30323D] shadow-lg p-5 flex justify-center items-center flex-col gap-4 rounded-md">
                 <h1 className="display-font font-semibold text-xl"><span className="text-red-500">Soon!</span> | Arifian<span className="text-red-500">.AI</span></h1>
-                <p>An AI chatbot that impersonating me!</p>
+                <p className="text-center">An AI chatbot that impersonating me!</p>
                 <p>See the development here: </p>
 
                 <a href="https://github.com/arifian853/arifian.ai" target="_blank" rel="noreferrer noopener"> <Button className="display-font font-semibold w-full flex flex-row justify-center items-center gap-1">Development Repository <HiOutlineExternalLink /></Button></a>
