3439c123d2e4ee30c0fc7457acfe771a26de7ebd
arifian853 <arifiansaputra43@gmail.com>
2024-11-20 21:57:34 +0700

    Added new updates

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base : 'https://arifian853.github.io/',
+  base : '/',
   plugins: [react()],
   resolve: {
     alias: {
